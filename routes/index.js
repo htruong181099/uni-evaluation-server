b99f2a61d628faf214b060753c55cf8626c868a1
Hoang Truong <hoangtruong181099@gmail.com>
2021-03-31 01:01:54 +0700

    add route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
+const AuthRouter = require("./auth.route");
+
 module.exports = (app)=>{
     app.get("/",(req,res,next)=>{
         res.json({
             message: "Hello, world!"
         })
     })
+
+    AuthRouter(app);
+
 }
