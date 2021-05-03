5f62133216e2ba51123edd87e4b2a0bf1ca446e2
Truong Hoang <hoangtruong181099@gmail.com>
2021-05-03 20:50:04 +0700

    fix

---
+++ config/formType.config.js
@@ -0,0 +1,16 @@
+formTypelist = [
+    {
+        code: "01",
+        name: "Giảng viên, Nghiên cứu viên, Kỹ sư phục vụ giảng dạy"
+    },
+    {
+        code: "02",
+        name: "Nhân viên hành chính, kỹ thuật, phục vụ, bảo vệ"
+    },
+    {
+        code: "03",
+        name: "Viên chức quản lý"
+    }
+]
+
+module.exports = formTypelist;
