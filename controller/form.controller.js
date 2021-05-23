3a05050bcd5c09ad3b185172f54166bfff82ad22
Truong Hoang <hoangtruong181099@gmail.com>
2021-05-23 16:57:45 +0700

    fix validate

--- controller/form.controller.js
+++ controller/form.controller.js
@@ -44,7 +44,7 @@ exports.validate = (method)=>{
         }
         case 'getEvaFormv2': {
             return [
-                param("ufcode","Invalid FormCode").exists().isMongoId()
+                param("ufid","Invalid FormCode").exists().isMongoId()
             ]
         }
         case 'getEvaFormbyID': {
